bc6495aa65f52c85768ea359d633b5058ab9b904
Carlos Moura <carlosmoura@live.com>
2022-12-24 15:33:22 -0300

    CRiação das interface IServerService e IMovieService.

---
+++ SeventhSeg.Application/Interfaces/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using SeventhSeg.Application.DTOs;
+
+namespace SeventhSeg.Application.Interfaces;
+
+public interface IMovieService
+{
+    Task<IEnumerable<MovieDTO>> GetMoviesAsync();
+    Task<IEnumerable<MovieDTO>> GetMoviesByServerIdAsync(Guid serverId);
+    Task<MovieDTO> GetByIdAsync(Guid id);
+    Task<MovieDTO> CreateAsync(MovieDTO movie);
+    Task<MovieDTO> UpdateAsync(MovieDTO movie);
+    Task<MovieDTO> RemoveAsync(MovieDTO movie);
+}
---
+++ SeventhSeg.Application/Interfaces/IServerService.cs
@@ -0,0 +1,12 @@
+﻿using SeventhSeg.Application.DTOs;
+
+namespace SeventhSeg.Application.Interfaces;
+
+public interface IServerService
+{
+    Task<IEnumerable<ServerDTO>> GetServersAsync();
+    Task<ServerDTO> GetByIdAsync(Guid id);
+    Task<ServerDTO> CreateAsync(ServerDTO server);
+    Task<ServerDTO> UpdateAsync(ServerDTO server);
+    Task<ServerDTO> RemoveAsync(ServerDTO server);
+}
