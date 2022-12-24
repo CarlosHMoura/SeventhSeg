72836b2038d011b166399ef906dd8b05038d78e5
Carlos Moura <carlosmoura@live.com>
2022-12-24 13:40:56 -0300

    Revert "Revert "Realizado criação do Dominio da aplicação.""
    This reverts commit 2040e3c2ae0200758b38acad431b48c386370c7e.

---
+++ SeventhSeg.Domain/Interfaces/IServerRepository.cs
@@ -0,0 +1,13 @@
+﻿using SeventhSeg.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace SeventhSeg.Domain.Interfaces;
+
+public interface IServerRepository
+{
+    Task<IEnumerable<Server>> GetServersAsync();
+    Task<Server> GetByIdAsync(string id);
+    Task<Server> CreateAsync(Server server);
+    Task<Server> UpdateAsync(Server server);
+    Task<Server> RemoveAsync(Server server);
+}
