cdb790f0ac33f2ae0df9a60115a1722f2073b2b0
Carlos Moura <carlosmoura@live.com>
2022-12-26 23:18:22 -0300

    Adicionado checagem e criação de diretorio para upload de videos.

--- SeventhSeg.API/Program.cs
+++ SeventhSeg.API/Program.cs
@@ -3,6 +3,11 @@ using SeventhSeg.Infra.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if(!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "MoviesUploaded")))
+{
+    Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "MoviesUploaded"));
+}
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
